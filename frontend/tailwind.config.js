c9eba74931c74fc1d7647299c6db5cc3f25c5643
QuantumlyTangled <ethoarx@gmail.com>
2021-04-23 22:21:36 +0200

    feat: some tailwindcss plugins

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,13 @@
 module.exports = {
-  purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: 'media', // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+	purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
+	darkMode: 'media', // or 'media' or 'class'
+	theme: {
+		extend: {
+			colors: require('daisyui/colors')
+		}
+	},
+	variants: {
+		extend: {}
+	},
+	plugins: [require('daisyui')]
+};

1b6a33109854ec7d3efc4cde85074d86185e373b
QuantumlyTangled <ethoarx@gmail.com>
2021-04-24 21:52:41 +0200

    feat: implement basics of Supabase

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,18 +1,28 @@
 import '@fullcalendar/common/main.css';
 import '@fullcalendar/daygrid/main.css';
 import '@fullcalendar/timegrid/main.css';
+import { createClient } from '@supabase/supabase-js';
+import { Auth } from '@supabase/ui';
 import NavBar from 'components/NavBar';
 import type { NextPage } from 'next';
 import type { AppProps } from 'next/app';
 import React from 'react';
 import 'styles/_App.css';
+import { SupabaseContextProvider } from 'use-supabase';
+
+// @ts-expect-error Stupidity
+const _supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
 
 const App: NextPage<AppProps> = ({ Component, pageProps }) => {
 	return (
 		<>
-			<NavBar />
-			<div className="py-8" />
-			<Component {...pageProps} />
+			<SupabaseContextProvider client={_supabase}>
+				<Auth.UserContextProvider supabaseClient={_supabase}>
+					<NavBar />
+					<div className="py-8" />
+					<Component {...pageProps} />
+				</Auth.UserContextProvider>
+			</SupabaseContextProvider>
 		</>
 	);
 };

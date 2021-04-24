b316ab8a6e7d3e657cc01f15f32f84b867e5596b
QuantumlyTangled <ethoarx@gmail.com>
2021-04-24 08:24:06 +0200

    feat: navbar

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,14 +1,17 @@
-import type { NextPage } from 'next';
-import type { AppProps } from 'next/app';
-import React from 'react';
 import '@fullcalendar/common/main.css';
 import '@fullcalendar/daygrid/main.css';
 import '@fullcalendar/timegrid/main.css';
+import NavBar from 'components/NavBar';
+import type { NextPage } from 'next';
+import type { AppProps } from 'next/app';
+import React from 'react';
 import 'styles/_App.css';
 
 const App: NextPage<AppProps> = ({ Component, pageProps }) => {
 	return (
 		<>
+			<NavBar />
+			<div className="py-8" />
 			<Component {...pageProps} />
 		</>
 	);

0e311a660743039e40279ee1d88cd54908d91563
QuantumlyTangled <ethoarx@gmail.com>
2021-04-24 06:26:18 +0200

    feat: make full calendar work

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,9 @@
 import type { NextPage } from 'next';
 import type { AppProps } from 'next/app';
 import React from 'react';
+import '@fullcalendar/common/main.css';
+import '@fullcalendar/daygrid/main.css';
+import '@fullcalendar/timegrid/main.css';
 import 'tailwindcss/tailwind.css';
 
 const App: NextPage<AppProps> = ({ Component, pageProps }) => {

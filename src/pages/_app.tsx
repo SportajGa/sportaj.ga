280edef8534e96927c03f3f1ddfcbb4d14211ed7
QuantumlyTangled <ethoarx@gmail.com>
2021-05-08 08:33:09 +0200

    fix: remove remainder of supabase

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,28 +1,18 @@
 import '@fullcalendar/common/main.css';
 import '@fullcalendar/daygrid/main.css';
 import '@fullcalendar/timegrid/main.css';
-import { createClient } from '@supabase/supabase-js';
-import { Auth } from '@supabase/ui';
 import NavBar from 'components/NavBar';
 import type { NextPage } from 'next';
 import type { AppProps } from 'next/app';
 import React from 'react';
 import 'styles/_App.css';
-import { SupabaseContextProvider } from 'use-supabase';
-
-// @ts-expect-error Stupidity
-export const _supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
 
 const App: NextPage<AppProps> = ({ Component, pageProps }) => {
 	return (
 		<>
-			<SupabaseContextProvider client={_supabase}>
-				<Auth.UserContextProvider supabaseClient={_supabase}>
-					<NavBar />
-					<div className="py-12" />
-					<Component {...pageProps} />
-				</Auth.UserContextProvider>
-			</SupabaseContextProvider>
+			<NavBar />
+			<div className="py-12" />
+			<Component {...pageProps} />
 		</>
 	);
 };

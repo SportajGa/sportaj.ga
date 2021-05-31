0594cc04f12fdfe600bf69e877e13a5b56798b90
QuantumlyTangled <ethoarx@gmail.com>
2021-05-31 22:50:05 +0200

    feat: use apollo provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,9 @@
+import { ApolloProvider } from '@apollo/client';
 import '@fullcalendar/common/main.css';
 import '@fullcalendar/daygrid/main.css';
 import '@fullcalendar/timegrid/main.css';
 import NavBar from 'components/NavBar';
+import { client } from 'core/apiClient';
 import type { NextPage } from 'next';
 import type { AppProps } from 'next/app';
 import React from 'react';
@@ -10,9 +12,11 @@ import 'styles/_App.css';
 const App: NextPage<AppProps> = ({ Component, pageProps }) => {
 	return (
 		<>
-			<NavBar />
-			<div className="py-12" />
-			<Component {...pageProps} />
+			<ApolloProvider client={client}>
+				<NavBar />
+				<div className="py-12" />
+				<Component {...pageProps} />
+			</ApolloProvider>
 		</>
 	);
 };

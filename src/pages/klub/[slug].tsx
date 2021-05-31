4681fa1468b35546b0a6062f0bfe1ad8cd0e5dd8
QuantumlyTangled <ethoarx@gmail.com>
2021-05-31 23:04:27 +0200

    feat: static probs generation

--- src/pages/klub/[slug].tsx
+++ src/pages/klub/[slug].tsx
@@ -1,10 +1,19 @@
 import { gql } from '@apollo/client';
+import type { Club } from '@sportajga/api';
 import { client, GraphQLResponse } from 'core/apiClient';
-import type { NextPage } from 'next';
+import type { GetStaticProps, NextPage } from 'next';
 import React from 'react';
 
-const KlubPage: NextPage = () => {
-	return <></>;
+export interface KlubProps {
+	name: string;
+}
+
+const KlubPage: NextPage<KlubProps> = ({ name }) => {
+	return (
+		<>
+			<h1>{name}</h1>
+		</>
+	);
 };
 
 const CLUB_SLUGS = gql`
@@ -14,6 +23,24 @@ const CLUB_SLUGS = gql`
 		}
 	}
 `;
+
+const GET_CLUB = gql`
+	query Club($slug: String!) {
+		club(slug: $slug) {
+			name
+		}
+	}
+`;
+
+export const getStaticProps: GetStaticProps<KlubProps, { slug: string }> = async ({ params }) => {
+	const { data } = await client.query<GraphQLResponse<'club'>>({ query: GET_CLUB, variables: { slug: params?.slug } });
+
+	return {
+		props: {
+			name: (data.club as Club).name
+		}
+	};
+};
 
 export async function getStaticPaths() {
 	const { data } = await client.query<GraphQLResponse<'allClubsBySlug'>>({ query: CLUB_SLUGS });

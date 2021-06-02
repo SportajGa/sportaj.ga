daa3325e66c35b483d2012e2076868c338f6679b
QuantumlyTangled <ethoarx@gmail.com>
2021-06-02 23:09:26 +0200

    feat: clubs in sitemap

--- src/core/sitemap.ts
+++ src/core/sitemap.ts
@@ -1,11 +1,15 @@
 import sitemapGenerator from 'nextjs-sitemap-generator';
 import path from 'path';
+import { allClubSlugs } from './clubs';
 
 export async function generateSitemap(srcPath: string) {
 	const baseUrl = 'https://sportaj.ga';
 
+	const slugs = await allClubSlugs();
+
 	await sitemapGenerator({
 		baseUrl,
+		extraPaths: slugs.map((slug) => `/klub/${slug}`),
 		pagesDirectory: path.join(srcPath, 'pages'),
 		targetDirectory: path.join(srcPath, 'public'),
 		nextConfigPath: path.join(srcPath, 'next.config.js'),

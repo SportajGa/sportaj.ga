d0f935fcdfc2e1078e9c68fdae719f09ea4a25ab
QuantumlyTangled <ethoarx@gmail.com>
2021-06-10 06:22:16 +0200

    feat: map page display names

--- src/pages/zemljevid.tsx
+++ src/pages/zemljevid.tsx
@@ -1,31 +1,45 @@
 import { gql } from '@apollo/client';
+import type { Club } from '@sportajga/api';
 import { client, GraphQLResponse } from 'core/apiClient';
 import type { GetStaticProps, NextPage } from 'next';
 import React from 'react';
 
-export interface ZemljevidProps {}
+export interface ZemljevidProps {
+	clubs: AllClubs;
+}
 
-const ZemljevidPage: NextPage<ZemljevidProps> = () => {
-	return <div>hi</div>;
+const ZemljevidPage: NextPage<ZemljevidProps> = ({ clubs }) => {
+	return (
+		<>
+			{clubs.map((c) => (
+				<div key={c.slug}>
+					<h1>{c.name}</h1>
+				</div>
+			))}
+		</>
+	);
 };
 
 const GET_ALL_CLUBS = gql`
-	query AllClubsFull() {
-		allClubs() {
+	query AllClubsFull {
+		allClubs {
 			name
-			description
-			location
-			locationFriendly
+			slug
 		}
 	}
 `;
 
+type AllClubs = Omit<readonly Pick<Club, 'name' | 'slug'>[], '__typename'>;
+
 export const getStaticProps: GetStaticProps<ZemljevidProps> = async () => {
-	const { data } = await client.query<GraphQLResponse<'club'>[]>({ query: GET_ALL_CLUBS });
+	const { data } = await client.query<GraphQLResponse<'allClubs'>>({ query: GET_ALL_CLUBS });
+	const clubs = data.allClubs as AllClubs;
 
 	return {
-		props: {},
-		revalidate: 120
+		props: {
+			clubs
+		},
+		revalidate: 30
 	};
 };
 

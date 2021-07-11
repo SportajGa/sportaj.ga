f99c323bef1a5e32216d047e03e5256bacd8d28b
Nejc Drobnič <ethoarx@gmail.com>
2021-07-11 16:55:52 +0200

    feat: allow content injection

--- src/pages/klub/[slug].tsx
+++ src/pages/klub/[slug].tsx
@@ -1,5 +1,5 @@
 import { gql } from '@apollo/client';
-import type { Club } from '@sportajga/api';
+import type { Club, ClubInjections } from '@sportajga/api';
 import ClubAddress from 'components/Club/ClubAddress';
 import ClubCalendar from 'components/Club/ClubCalendar';
 import ClubDescription from 'components/Club/ClubDescription';
@@ -7,18 +7,22 @@ import Loading from 'components/Loading';
 import { client, GraphQLResponse } from 'core/apiClient';
 import { allClubSlugs } from 'core/clubs';
 import GoogleMapReact from 'google-map-react';
+import Interweave from 'interweave';
 import type { GetStaticProps, NextPage } from 'next';
+import { NextSeo } from 'next-seo';
 import { useRouter } from 'next/dist/client/router';
-import React, { useState, useEffect } from 'react';
+import Head from 'next/head';
+import React, { useEffect, useState } from 'react';
 
 export interface KlubProps {
 	name: string;
 	description: string | null;
 	location: string | null;
 	locationFriendly: string | null;
+	injections: Pick<ClubInjections, 'head' | 'pageEnd'> | null;
 }
 
-const KlubPage: NextPage<KlubProps> = ({ name, description, location, locationFriendly }) => {
+const KlubPage: NextPage<KlubProps> = ({ name, description, location, locationFriendly, injections }) => {
 	const router = useRouter();
 	const [latLon, setLatLon] = useState<{ lat: number; lon: number }>();
 
@@ -35,53 +39,67 @@ const KlubPage: NextPage<KlubProps> = ({ name, description, location, locationFr
 	}
 
 	return (
-		<div className="container">
-			<div className="flex flex-wrap">
-				<div className="content-center shadow-lg w-2/6 hidden md:pr-2 md:block">
-					<figure className="px-10 pt-10">
-						<img src="https://sportaj.ga/media/klub_logo/5_1.webp" className="rounded-xl m-auto" height="128" width="128" />
-					</figure>
-					{locationFriendly && (
-						<div className="card">
+		<>
+			<NextSeo //
+				title={name}
+				description={description ?? undefined}
+			/>
+
+			{injections && injections.head ? (
+				<Head>
+					<Interweave content={injections.head} />
+				</Head>
+			) : null}
+
+			<div className="container">
+				<div className="flex flex-wrap">
+					<div className="content-center shadow-lg w-2/6 hidden md:pr-2 md:block">
+						<figure className="px-10 pt-10">
+							<img src="https://sportaj.ga/media/klub_logo/5_1.webp" className="rounded-xl m-auto" height="128" width="128" />
+						</figure>
+						{locationFriendly && (
+							<div className="card">
+								<div className="card-body">
+									<ClubAddress address={locationFriendly} />
+								</div>
+							</div>
+						)}
+						{location && latLon && (
+							<div className="h-96">
+								<GoogleMapReact
+									yesIWantToUseGoogleMapApiInternals={true}
+									center={{
+										lat: latLon!.lat,
+										lng: latLon!.lon
+									}}
+									zoom={11}
+								></GoogleMapReact>
+							</div>
+						)}
+					</div>
+					<div className="w-full shadow-lg md:pl-2 md:w-4/6">
+						<figure className="px-10 pt-10">
+							<img className="w-full h-auto rounded-md" src="https://sportaj.ga/media/header_slike/5_qfdjGtZ.webp" />
+						</figure>
+						<div className="card px-10">
 							<div className="card-body">
-								<ClubAddress address={locationFriendly} />
+								<span className="card-title text-center">{name}</span>
+								{locationFriendly && <ClubAddress address={locationFriendly} className="md:hidden text-sm" />}
+								<div className="flex flex-col py-4">
+									<section className="flex justify-between">
+										<p>1</p>
+										<p>2</p>
+									</section>
+								</div>
+								<ClubDescription description={description} />
+								<ClubCalendar className="hidden md:block" />
 							</div>
-						</div>
-					)}
-					{location && latLon && (
-						<div className="h-96">
-							<GoogleMapReact
-								yesIWantToUseGoogleMapApiInternals={true}
-								center={{
-									lat: latLon!.lat,
-									lng: latLon!.lon
-								}}
-								zoom={11}
-							></GoogleMapReact>
-						</div>
-					)}
-				</div>
-				<div className="w-full shadow-lg md:pl-2 md:w-4/6">
-					<figure className="px-10 pt-10">
-						<img className="w-full h-auto rounded-md" src="https://sportaj.ga/media/header_slike/5_qfdjGtZ.webp" />
-					</figure>
-					<div className="card px-10">
-						<div className="card-body">
-							<span className="card-title text-center">{name}</span>
-							{locationFriendly && <ClubAddress address={locationFriendly} className="md:hidden text-sm" />}
-							<div className="flex flex-col py-4">
-								<section className="flex justify-between">
-									<p>1</p>
-									<p>2</p>
-								</section>
-							</div>
-							<ClubDescription description={description} />
-							<ClubCalendar className="hidden md:block" />
 						</div>
 					</div>
 				</div>
+				{injections && injections.pageEnd ? <Interweave content={injections.pageEnd} /> : null}
 			</div>
-		</div>
+		</>
 	);
 };
 
@@ -92,20 +110,25 @@ const GET_CLUB = gql`
 			description
 			location
 			locationFriendly
+			injections {
+				head
+				pageEnd
+			}
 		}
 	}
 `;
 
 export const getStaticProps: GetStaticProps<KlubProps, { slug: string }> = async ({ params }) => {
 	const { data } = await client.query<GraphQLResponse<'club'>>({ query: GET_CLUB, variables: { slug: params?.slug } });
-	const club = data.club as Pick<Club, 'name' | 'description' | 'location' | 'locationFriendly'>;
+	const club = data.club as Pick<Club, 'name' | 'description' | 'location' | 'locationFriendly' | 'injections'>;
 
 	return {
 		props: {
 			name: club.name,
 			description: club.description ?? null,
 			location: club.location ?? null,
-			locationFriendly: club.locationFriendly ?? null
+			locationFriendly: club.locationFriendly ?? null,
+			injections: club.injections ?? null
 		},
 		revalidate: 120
 	};

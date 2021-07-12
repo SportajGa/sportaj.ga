099509aa87eec0b4287d14622c8eeb5afe68589d
Nejc Drobnič <ethoarx@gmail.com>
2021-07-12 22:07:27 +0200

    feat: privacy policy

---
+++ src/pages/privacy.tsx
@@ -0,0 +1,214 @@
+import React from 'react';
+import type { NextPage } from 'next';
+
+const PrivacyPage: NextPage = () => {
+	return (
+		<>
+			<div className="container">
+				<div className="p-16 mt-8">
+					<div className="grid grid-flow-row dark:text-gray-200">
+						<h1 className="text-2xl font-bold text-center">Privacy Policy</h1>
+						<p className="text-center dark:text-gray-300">Last updated on the 12th of July 2021. Effective as of the 13th of July 2021</p>
+					</div>
+				</div>
+			</div>
+
+			<div className="p-8 dark:text-gray-200 bg-dark-alt">
+				<div className="container prose prose-md">
+					{/* <p>
+						PenguBot provides a chat bot for the <a href="https://discord.com">Discord</a> chat platform, (the "Bot"), a website that
+						serves as a dashboard (the "Site") and various related services (collectively, the "Service(s)"). The Services are operated by
+						PenguBot (the "Company", "we" or "us") for users of the Service ("you" or the "user(s)"). This privacy policy sets forth our
+						policy with respect to information that is collected from visitors to the Site and users of the Bot and/or the Services. Under
+						applicable law, PenguBot is the "data controller" of user data collected through the Services.
+					</p> */}
+
+					<h3>Data We Collect</h3>
+					<p>
+						When you interact with us through the Services, we may collect information from you, as further described below: Information
+						You Provide: We collect information from you when you voluntarily provide us with the information, such as when you use any of
+						the Services.
+						{/* Information we collect may include but is not be limited to cookies, usage data and, usernames, or other content
+						you send via the chat feature in Discord when interacting with the Bot. */}
+					</p>
+					<p>
+						We may use third party web site analytics tools such as but not limited to Google Analytics on our Site that employ cookies to
+						collect certain information concerning your use of our Services. However, you can disable cookies by changing your browser
+						settings. Further information about the procedure to follow in order to disable cookies can be found on your Internet browser
+						provider's website via your help screen.
+					</p>
+					<p>
+						When you interact with us through the Services, we receive and store certain information such as an IP address, device ID and
+						your activities within the Services. We may store such information or such information may be included in the databases owned
+						and maintained by affiliates, agents or service providers. The Services may use such information and pool it with other
+						information to track, for example, the total number of visitors to our Site, the number of messages users have sent, as well
+						as the sites which refers visitors to our Services.
+					</p>
+					<p>
+						We employ cookies and similar technologies to keep track of your local settings such as which account you have used to log
+						into the Site.
+					</p>
+
+					<h3>Data Retention</h3>
+					<p>
+						We retain personal data for so long as it may be relevant to the purposes identified but not limited to herein. To dispose off
+						personal data, we may anonymize, delete or take other appropriate steps for it. Data may persist in copies made for backup for
+						additional time.
+					</p>
+
+					<h3>Disclousure of Information</h3>
+					<p>
+						The Company is not in the business of selling your information. There are, however, certain circumstances in which we may
+						share your information with certain third parties, as set forth below:{' '}
+						<li>Consent: We may transfer your information with your consent.</li>
+						<li>
+							Consultants and outside collaborators: Like many businesses, we sometimes request the help of other companies or
+							individuals to perform certain functions. Examples of such functions include accessing, modifying and adding to the
+							open-source source code. In order to perform these functions they may require samples of any of the data stored by the
+							Services. These people will however never have access to the full data and any data will always be anonymized where
+							required.
+						</li>
+						<li>
+							Legal Requirements: We may disclose your information if required to do so by law or in the good faith belief that such
+							action is necessary to (i) comply with a legal obligation, (ii) protect and defend the rights or property of the Company
+							or Related Companies, (iii) protect the personal safety of users of the Services or the public, or (iv) protect against
+							legal liability.
+						</li>
+						<h3>Unsolicited Information</h3>
+						<p>
+							You may provide us with ideas for new products or modifications to existing products, and other unsolicited submissions
+							(collectively, “Unsolicited Information”). All Unsolicited Information shall be deemed to be non-confidential and we shall
+							be free to reproduce, use, disclose, and distribute such Unsolicited Information to others without limitation or
+							attribution.
+						</p>
+						<h3>Data Processing</h3>
+						<p>
+							The Company is based in Slovenia, the Services provided and the data is hosted on the servers at Srednja
+							elektro-računalniška šola Maribor, Smetanova ulica 6, 2000 Maribor, Slovenia. No matter where you are located you consent
+							to processing and transferring of your information in and to Germany, Slovenia, and other countries. The laws of Germany,
+							Slovenia, and other countries governing data collection and use may not be as comprehensive or protective as the laws of
+							the country where you live.
+						</p>
+						<h3>Children</h3>
+						<p>
+							Our Services are for users age 13 and over and we do not knowingly collect personal information from children under the
+							age of 13. If you are a parent or guardian of a child under the age of 13 and believe he or she has disclosed personal
+							information to us please contact us at <a href="mailto:contact@sportaj.ga">contact@sportaj.ga</a>. Note: In some
+							countries, the age of digital consent is older than 13. If you are in those countries, you must be at least that age to
+							use the Services. For example, for residents of the EEA, where processing of personal information is based on consent,
+							SportajGa will not knowingly engage in that processing for users under the age of consent established by applicable data
+							protection law. If we learn that we are engaged in that processing with such users, we will halt such processing and will
+							take reasonable measures to promptly remove applicable information from our records.
+						</p>
+						<h3>Use of Information</h3>
+						<p>
+							We use the information you provide in a manner that is consistent with this Privacy Policy. If you provide information for
+							a certain reason, we may use the information in connection with the reason for which it was provided. For instance, if you
+							contact us by email, we will use the information you provide to answer your question or resolve your problem. Also, if you
+							provide information in order to obtain access to the Services, we will use your information to provide you with access to
+							such services and to monitor your use of such services. The Company and its subsidiaries and affiliates (the “Related
+							Companies”) may also use your information collected through the Services to help us improve the content and functionality
+							of the Services, to better understand our users and to improve the Services. The Company and its affiliates may use this
+							information to contact you in the future to tell you about services we believe will be of interest to you. If we do so,
+							each marketing communication we send you will contain instructions permitting you to "opt-out" of receiving future
+							marketing communications. In addition, if at any time you wish not to receive any future marketing communications or you
+							wish to have your name deleted from our mailing lists, please contact us as indicated below.
+						</p>
+						<h3>Security</h3>
+						<p>
+							We take reasonable steps to protect the information provided via the Services from loss, misuse, and unauthorized access,
+							disclosure, alteration, or destruction. However, no Internet or email transmission is ever fully secure or error free. In
+							particular, email sent to or from the Services may not be secure. Therefore, you should take special care in deciding what
+							information you send to us via email. Please keep this in mind when disclosing any information via the Internet.
+						</p>
+						<h3>Data Rights</h3>
+						<p>
+							We believe that users should be treated equally no matter where they are, and so we are making the following options to
+							control your data available to all users, regardless of their location. Individuals in California, the European Economic
+							Area, Canada, Costa Rica and some other jurisdictions have certain legal rights to obtain confirmation of whether we hold
+							personal data about them, to access personal data we hold about (including, in some cases, in portable form), and to
+							obtain its correction, update, amendment or deletion in appropriate circumstances. They may also object to our uses or
+							disclosures of personal data, to request a restriction on its processing, or withdraw any consent, though such actions
+							typically will not have retroactive effect. They also will not affect our ability to continue processing data in lawful
+							ways.
+						</p>
+						<p>
+							If you would like to submit a data access request, you can do so by sending an email to{' '}
+							<a href="mailto:contact@sportaj.ga">contact@sportaj.ga</a>. Upon request we will start the process and provide you a link
+							to access the personal data the Services has about you within 30-45 standard working days.
+						</p>
+						<p>
+							You can request modifications to your data from us directly. Please write us at{' '}
+							<a href="mailto:contact@sportaj.ga">contact@sportaj.ga</a> with the words "Personal Data Request" in the subject or body
+							of your message, along with an explanation of what data subject right you are seeking to exercise. For your protection, we
+							may take steps to verify identity before responding to your request.
+						</p>
+						<h3>California Consumer Privacy Act</h3>
+						<p>
+							Consumers residing in California are afforded certain additional rights with respect to their personal information under
+							the California Consumer Privacy Act or (“CCPA”) and the “Shine the Light” Law. If you are a California resident, this
+							section applies to you.
+						</p>
+						<h4>California Consumer Privacy Act</h4>
+						<p>
+							Our Collection and Use of Personal Information: We collect the following categories of personal information: internet or
+							other network information (how you interact with the application); location information (because your IP address may
+							indicate your general location); and other information that identifies or can be reasonably associated with you. For
+							examples of the precise data points we collect and the sources of such collection, please see the "Data We Collect”
+							section above. We collect personal information for the business and commercial purposes described in "Use of Information”
+							above.
+						</p>
+						<p>
+							Disclosure of Personal Information: We may share your personal information with third parties as described in the
+							“Disclosure Of Information” section above. We disclose the categories of personal information mentioned above for business
+							or commercial purposes.
+						</p>
+						<p>
+							No Sale of Personal Information: The CCPA sets forth certain obligations for businesses that sell personal information. We
+							do not sell the personal information of our users.
+						</p>
+						<p>
+							Exercising Your Consumer Rights: If you are a California resident, you have the right to request (1) more information
+							about the categories and specific pieces of personal information we have collected and disclosed for a business purpose in
+							the last 12 months, (2) deletion of your personal information, and (3) to opt out of sales of your personal information,
+							if applicable. Details on how to make these requests are in the “Data Rights” section above. We will not discriminate
+							against you if you exercise your rights under the CCPA.
+						</p>
+						<h3>Data Protection Officer</h3>
+						<p>
+							In respect of the data of our users generated and saved from the website, and other related services, the Data Protection
+							Officer for the company is Nejc Drobnič (<a href="mailto:nejc@sportaj.ga">nejc@sportaj.ga</a>).
+						</p>
+						<h3>External Links</h3>
+						<p>
+							This Privacy Policy applies only to the Services. The Services may contain links to other web sites and products not
+							operated or controlled by us (the “Third Party Sites”). The policies and procedures we have described here do not apply to
+							the Third Party Sites. The links from the Services do not imply that we endorse or have reviewed the Third Party Sites. We
+							suggest contacting those sites directly for information on their privacy policies.
+						</p>
+						<h3>Third Parties</h3>
+						<p>
+							The Bot and Services are built collectively from the use of third-party tools and thus enforces their own policies to be
+							applicable to the users along with this one. We use Google's Services for which the policy can be found{' '}
+							<a href="https://policies.google.com/privacy">here</a>.
+						</p>
+						<h3>Changes to Policy</h3>
+						<p>
+							We reserve the right to update or modify this Privacy Policy at any time and from time to time without prior notice.
+							Please review this policy periodically, and especially before you provide any information. This Privacy Policy was last
+							updated on the date indicated above. Your continued use of the Services after any changes or revisions to this Privacy
+							Policy shall indicate your agreement with the terms of such revised Privacy Policy.
+						</p>
+						<h3>Contact Us</h3>
+						<p>
+							You may feel free to contact us regarding any questions related to your information and privacy policy at{' '}
+							<a href="mailto:contact@sportaj.ga">contact@sportaj.ga</a>.
+						</p>
+					</p>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default PrivacyPage;

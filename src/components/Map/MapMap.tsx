ba018eb521dc72fe1054513ff351a737c79e503d
Nejc Drobnič <ethoarx@gmail.com>
2021-08-03 17:35:56 +0200

    refactor: use hasura action

--- src/components/Map/MapMap.tsx
+++ src/components/Map/MapMap.tsx
@@ -1,23 +1,26 @@
-import axios from 'axios';
-import type * as GeoJSON from 'geojson';
+import { gql, useQuery } from '@apollo/client';
+import type { GeoJsonFormOut } from '@sportajga/mappings';
+import type { GraphQLResponse } from 'core/apiClient';
 import React from 'react';
 import ReactMapGL, { FullscreenControl, GeolocateControl, Layer, Source } from 'react-map-gl';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectViewport, setViewport } from 'state/reducers/map';
-import useSWR from 'swr';
 
 export interface MapMapProps {}
 
-const fetcher = (url: string) => axios.get(url).then((res) => res.data);
+const GET_CLUB_CLUSTERS = gql`
+	query ClusterPoints {
+		geojson(filter: { name: "", slug: "" }) {
+			data
+		}
+	}
+`;
 
 const MapMap: React.FC<MapMapProps> = () => {
 	const viewport = useSelector(selectViewport);
 	const dispatch = useDispatch();
 
-	const { data } = useSWR<GeoJSON.FeatureCollection<GeoJSON.Geometry>>('/api/data/clubs', fetcher, {
-		refreshWhenHidden: false,
-		refreshInterval: 10000
-	});
+	const { data, loading } = useQuery<GraphQLResponse<'geojson'>>(GET_CLUB_CLUSTERS);
 
 	return (
 		<>
@@ -30,18 +33,20 @@ const MapMap: React.FC<MapMapProps> = () => {
 				height="100%"
 				onViewportChange={(viewport: unknown) => dispatch(setViewport(viewport as any))}
 			>
-				<FullscreenControl className="right-16 top-4" />
+				<FullscreenControl className="right -16 top-4" />
 				<GeolocateControl className="right-4 top-4" positionOptions={{ enableHighAccuracy: true }} trackUserLocation={true} auto={true} />
-				<Source id="clubs" type="geojson" data={data}>
-					<Layer
-						id="point"
-						type="circle"
-						paint={{
-							'circle-radius': 10,
-							'circle-color': '#007cbf'
-						}}
-					/>
-				</Source>
+				{!loading && (
+					<Source id="clubs" type="geojson" data={JSON.parse((data!.geojson as GeoJsonFormOut).data)}>
+						<Layer
+							id="point"
+							type="circle"
+							paint={{
+								'circle-radius': 10,
+								'circle-color': '#007cbf'
+							}}
+						/>
+					</Source>
+				)}
 			</ReactMapGL>
 		</>
 	);

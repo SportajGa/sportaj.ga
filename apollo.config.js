7411102c6cc0eecf755a8c6893edaa3b4f723bb9
Nejc Drobnič <ethoarx@gmail.com>
2021-08-03 17:36:11 +0200

    fix: use new typings

--- apollo.config.js
+++ apollo.config.js
@@ -1,7 +1,6 @@
 module.exports = {
 	client: {
 		service: {
-			name: 'sportaj-api',
 			localSchemaFile: './node_modules/@sportajga/mappings/generated/ts/api.graphql'
 		}
 	}
--- src/core/apiClient.ts
+++ src/core/apiClient.ts
@@ -1,9 +1,9 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
-import type { Query } from '@sportajga/api';
+import type { Query_Root } from '@sportajga/mappings';
 
 export const client = new ApolloClient({
 	uri: 'https://api.sportaj.ga',
 	cache: new InMemoryCache()
 });
 
-export type GraphQLResponse<K extends keyof Omit<Query, '__typename'>> = Record<K, Omit<Query[K], '__typename'>>;
+export type GraphQLResponse<K extends keyof Omit<Query_Root, '__typename'>> = Record<K, Omit<Query_Root[K], '__typename'>>;

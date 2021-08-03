51eecbbaba1583608303fd3324645e4cf4fe1b85
Nejc Drobnič <ethoarx@gmail.com>
2021-08-03 22:26:02 +0200

    refactor: make util use new api

--- src/core/clubs.ts
+++ src/core/clubs.ts
@@ -3,14 +3,14 @@ import { client, GraphQLResponse } from 'core/apiClient';
 
 const CLUB_SLUGS = gql`
 	query AllClubs {
-		allClubsBySlug {
+		club {
 			slug
 		}
 	}
 `;
 
 export const allClubSlugs = async () => {
-	const { data } = await client.query<GraphQLResponse<'allClubsBySlug'>>({ query: CLUB_SLUGS });
+	const { data } = await client.query<GraphQLResponse<'club'>>({ query: CLUB_SLUGS });
 
-	return data.allClubsBySlug.map(({ slug }) => slug);
+	return data.club.map(({ slug }) => slug);
 };

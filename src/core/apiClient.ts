8f17baddb8536ceb107a32cf606929a4fbe54c74
Nejc Drobnič <ethoarx@gmail.com>
2021-08-03 22:27:12 +0200

    fix: switch to new url

--- src/core/apiClient.ts
+++ src/core/apiClient.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 import type { Query_Root } from '@sportajga/mappings';
 
 export const client = new ApolloClient({
-	uri: 'https://api.sportaj.ga',
+	uri: 'https://hsaura.sportaj.ga/v1/graphql',
 	cache: new InMemoryCache()
 });
 

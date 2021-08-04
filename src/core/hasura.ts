d70706013c9508a685a2be0fc48f70b7796dc743
Nejc Drobnič <ethoarx@gmail.com>
2021-08-04 11:47:59 +0200

    chore: debug

--- src/core/hasura.ts
+++ src/core/hasura.ts
@@ -3,7 +3,12 @@ import type { Account, Profile, User } from 'next-auth';
 export function formHasuraJWTPayload(_?: User, account?: Account, __?: Profile) {
 	const data = new Map();
 
+	console.log('account', account);
+
 	if (account?.provider === 'facebook') data.set('X-Hasura-User-FB-Id', account.id);
+
+	console.log('data map', data);
+	console.log('object', Object.fromEntries(data));
 
 	return Object.fromEntries(data);
 }

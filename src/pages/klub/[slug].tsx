44da000a377223a240d458d28fff72d808afa435
Nejc Drobnič <ethoarx@gmail.com>
2021-08-05 19:39:08 +0200

    feat: not found placeholder

--- src/pages/klub/[slug].tsx
+++ src/pages/klub/[slug].tsx
@@ -13,9 +13,11 @@ import { NextSeo } from 'next-seo';
 import { useRouter } from 'next/dist/client/router';
 import dynamic from 'next/dynamic';
 import React, { useEffect, useState } from 'react';
+import { Else, If, Then } from 'react-if';
 import ReactPlaceholder from 'react-placeholder/lib';
 
 export interface KlubProps {
+	pathSlug: string;
 	slug: string | null;
 	name: string | null;
 	description: string | null;
@@ -33,7 +35,7 @@ const ClubMap = dynamic(() => import('components/Club/ClubMap'), {
 	)
 });
 
-const KlubPage: NextPage<KlubProps> = ({ slug, name, description, location, locationFriendly, logo }) => {
+const KlubPage: NextPage<KlubProps> = ({ pathSlug, slug, name, description, location, locationFriendly, logo }) => {
 	const router = useRouter();
 	const [latLon, setLatLon] = useState<LatLon>();
 
@@ -51,54 +53,66 @@ const KlubPage: NextPage<KlubProps> = ({ slug, name, description, location, loca
 
 	return (
 		<>
-			<NextSeo //
-				title={name ?? slug ?? ''}
-				description={description ?? undefined}
-			/>
-			<Offset />
-			<div className="container pb-8 px-4 md:px-0">
-				<div className="flex flex-wrap">
-					<div className="content-center w-7/24 hidden md:block">
-						<div className="bg-white rounded-md shadow-lg">
-							<figure className="px-10 pt-10">
-								<ClubLogo logo={logo} name={name} />
-							</figure>
-							{locationFriendly && (
-								<div className="card">
-									<div className="card-body">
-										<ClubAddress address={locationFriendly} />
-									</div>
-								</div>
-							)}
-							{location && latLon && (
-								<div className="h-96 p-4">
-									<ClubMap latlon={latLon} title={name ?? slug ?? ''} />
-								</div>
-							)}
+			{/* TODO: Convert into screens/views */}
+			<If condition={slug !== null}>
+				<Else>
+					<div className="flex h-screen">
+						<div className="m-auto">
+							Can't find <code>{pathSlug}</code>
 						</div>
 					</div>
-					<div className="md:w-1/24" />
-					<div className="w-full bg-white rounded-md shadow-lg md:w-15/24">
-						<figure className="px-10 pt-10">
-							<img className="w-full h-auto rounded-md" src="https://storage.googleapis.com/sportaj-cdn/headers/5.jpg" />
-						</figure>
-						<div className="card px-10">
-							<div className="card-body">
-								<span className="card-title text-center">{name}</span>
-								{locationFriendly && <ClubAddress address={locationFriendly} className="md:hidden text-sm" />}
-								<div className="flex flex-col py-4">
-									<section className="flex justify-between">
-										<p>1</p>
-										<p>2</p>
-									</section>
+				</Else>
+				<Then>
+					<NextSeo //
+						title={name ?? slug ?? ''}
+						description={description ?? undefined}
+					/>
+					<Offset />
+					<div className="container pb-8 px-4 md:px-0">
+						<div className="flex flex-wrap">
+							<div className="content-center w-7/24 hidden md:block">
+								<div className="bg-white rounded-md shadow-lg">
+									<figure className="px-10 pt-10">
+										<ClubLogo logo={logo} name={name} />
+									</figure>
+									{locationFriendly && (
+										<div className="card">
+											<div className="card-body">
+												<ClubAddress address={locationFriendly} />
+											</div>
+										</div>
+									)}
+									{location && latLon && (
+										<div className="h-96 p-4">
+											<ClubMap latlon={latLon} title={name ?? slug ?? ''} />
+										</div>
+									)}
 								</div>
-								<ClubDescription description={description} />
-								<ClubCalendar className="hidden md:block" />
+							</div>
+							<div className="md:w-1/24" />
+							<div className="w-full bg-white rounded-md shadow-lg md:w-15/24">
+								<figure className="px-10 pt-10">
+									<img className="w-full h-auto rounded-md" src="https://storage.googleapis.com/sportaj-cdn/headers/5.jpg" />
+								</figure>
+								<div className="card px-10">
+									<div className="card-body">
+										<span className="card-title text-center">{name}</span>
+										{locationFriendly && <ClubAddress address={locationFriendly} className="md:hidden text-sm" />}
+										<div className="flex flex-col py-4">
+											<section className="flex justify-between">
+												<p>1</p>
+												<p>2</p>
+											</section>
+										</div>
+										<ClubDescription description={description} />
+										<ClubCalendar className="hidden md:block" />
+									</div>
+								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-			</div>
+				</Then>
+			</If>
 		</>
 	);
 };
@@ -122,6 +136,7 @@ export const getStaticProps: GetStaticProps<KlubProps, { slug: string }> = async
 
 	return {
 		props: {
+			pathSlug: params?.slug ?? '',
 			slug: club?.slug ?? null,
 			name: club?.name ?? null,
 			description: club?.description ?? null,

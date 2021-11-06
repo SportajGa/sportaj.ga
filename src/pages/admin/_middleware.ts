b6e1705d29b0c415dded8e359720d47d52230464
Nejc Drobnič <ethoarx@gmail.com>
2021-11-06 06:49:20 +0100

    fix: ignore types for req

--- src/pages/admin/_middleware.ts
+++ src/pages/admin/_middleware.ts
@@ -2,6 +2,7 @@ import { NextRequest, NextResponse } from 'next/server';
 import { getSession } from 'next-auth/client';
 
 export async function middleware(req: NextRequest) {
+	// @ts-expect-error Heh
 	const session = await getSession({ req });
 	const currentPath = `${process.env.BASE_URL}${req.nextUrl.href}`;
 
